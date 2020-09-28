1a7d6fe17a8e26c08c6714ed975b0806c6918ff8
clgarillo <christian.garillo@monstar-lab.com>
2020-09-28 17:34:26 +0800

    Setup Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,11 +19,29 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        body: Text("Hello Body"),
-        appBar: AppBar(
-          title: Text("Hello Appbar"),
-        ),
+          appBar: AppBar(title: Text("Hello AppBar")),
+          body:
+          Container(
+            child: ListView(
+              children: [
+                Text("Hello"),
+                Text("World"),
+                RaisedButton(child: Text("Button"), onPressed: showTestData, ),
+              ],
+            ),
+          ),
       ),
     );
+  }
+
+  QuerySnapshot showTestData() {
+    FirebaseFirestore.instance
+        .collection('users')
+        .snapshots()
+        .listen((QuerySnapshot snapshot) {
+          snapshot.docs.forEach((element) {
+            print(element.data()["full_name"]);
+          });
+    });
   }
 }
